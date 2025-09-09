248eaae1c76b6686649ff51ed8b3a80718f7ee8c
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-09 19:27:14 +0500

    updates

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,20 +1,41 @@
-import { Box, Grid, Paper } from "@mui/material";
+import { Box, Grid, Paper, Typography, Button } from "@mui/material";
 import useHabitStore from "../store/store";
 
-
 const HabitList = () => {
-    const {habits} = useHabitStore(); 
-  return <Box sx={{display: 'flex', flexDirection: 'column', gap: 2, mt: 2}}>
-    {
-    habits.map((habit) =>(
-        <Paper key={habit.id}  elevation={2} sx={{p:2}}>
-          <Grid container  alignItems="center">
+  const { habits } = useHabitStore();
 
+    const today = new Date().toISOString().split("T")[0];
 
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}>
+      {habits.map((habit) => (
+        <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
+          <Grid container alignItems="center">
+            <Grid item xs={12} sm={6}>
+              <Typography variant="h6">{habit.name}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Frequency: {habit.frequency}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+                <Button variant="outlined"
+                color={
+                  habit.completedDates.includes(today) ? "success" : "primary"
+                }>
+
+                  {
+                    habit.completedDates.includes(today) ? "Completed Today" : "Mark as Completed"
+                  }
+                </Button>
+                <Button color="error" variant="outlined">Remove</Button>
+              </Box>
+            </Grid>
           </Grid>
         </Paper>
-    ))
-    }
-  </Box>;
-}
+      ))}
+    </Box>
+  );
+};
+
 export default HabitList;
